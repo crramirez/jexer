e2361d97439d2399113f3acf86516c9055b3692c
Carlos Ramirez <crramirez@gmail.com>
2025-12-02 17:27:17 +0100

    - Introduce `DemoApplicationCore` and `DemoCoreMainWindow` to extend Jexer demo functionality with multiple themes, multilingual support, and enhanced UI elements. - Add Spanish localization via `.properties` files for `DemoApplicationCore` and `DemoCoreMainWindow`. - Modify `pom.xml` to exclude new demo-related classes from the library build. - Refactor existing demo structure to support the new core application layout.

---
+++ src/demo/DemoApplicationCore.java
@@ -0,0 +1,370 @@
+/*
+ * Jexer - Java Text User Interface
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2025 Autumn Lamonte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * @author Autumn Lamonte ♥
+ * @version 1
+ */
+package demo;
+
+import jexer.*;
+import jexer.backend.Backend;
+import jexer.backend.ECMA48Terminal;
+import jexer.backend.SwingTerminal;
+import jexer.event.TMenuEvent;
+import jexer.menu.TMenu;
+import jexer.menu.TMenuItem;
+import jexer.menu.TSubMenu;
+
+import java.io.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * The demo application itself.
+ */
+public class DemoApplicationCore extends TApplication {
+
+    // ------------------------------------------------------------------------
+    // Variables --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Translated strings.
+     */
+    private ResourceBundle i18n = ResourceBundle.getBundle(DemoApplicationCore.class.getName());
+
+    // ------------------------------------------------------------------------
+    // Constructors -----------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Public constructor.
+     *
+     * @param input the InputStream underlying 'reader'.  Its available()
+     * method is used to determine if reader.read() will block or not.
+     * @param reader a Reader connected to the remote user.
+     * @param writer a PrintWriter connected to the remote user.
+     * @param setRawMode if true, set System.in into raw mode with stty.
+     * This should in general not be used.  It is here solely for Demo3,
+     * which uses System.in.
+     * @throws IllegalArgumentException if input, reader, or writer are null.
+     */
+    @SuppressWarnings("this-escape")
+    public DemoApplicationCore(final InputStream input, final Reader reader,
+                               final PrintWriter writer, final boolean setRawMode) {
+        super(input, reader, writer, setRawMode);
+        addAllWidgets();
+
+        getBackend().setTitle(i18n.getString("applicationTitle"));
+    }
+
+    // ------------------------------------------------------------------------
+    // TApplication -----------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Handle menu events.
+     *
+     * @param menu menu event
+     * @return if true, the event was processed and should not be passed onto
+     * a window
+     */
+    @Override
+    public boolean onMenu(final TMenuEvent menu) {
+
+        if (menu.getId() == 2050) {
+            new TEditColorThemeWindow(this);
+            return true;
+        }
+
+        if (menu.getId() == TMenu.MID_OPEN_FILE) {
+            try {
+                String filename = fileOpenBox(".");
+                 if (filename != null) {
+                     try {
+                         new TEditorWindow(this, new File(filename));
+                     } catch (IOException e) {
+                         e.printStackTrace();
+                     }
+                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        if (menu.getId() == 10000) {
+            new DemoMainWindow(this);
+            return true;
+        }
+
+        if (menu.getId() == 10001) {
+            // Look cute: switch the color theme, window borders, and button
+            // styles.
+            System.setProperty("jexer.TWindow.borderStyleForeground", "round");
+            System.setProperty("jexer.TWindow.borderStyleModal", "round");
+            System.setProperty("jexer.TWindow.borderStyleMoving", "round");
+            System.setProperty("jexer.TWindow.borderStyleInactive", "round");
+            System.setProperty("jexer.TEditColorTheme.borderStyle", "round");
+            System.setProperty("jexer.TEditColorTheme.options.borderStyle", "round");
+            System.setProperty("jexer.TRadioGroup.borderStyle", "round");
+            System.setProperty("jexer.TScreenOptions.borderStyle", "round");
+            System.setProperty("jexer.TScreenOptions.grid.borderStyle", "round");
+            System.setProperty("jexer.TScreenOptions.options.borderStyle", "round");
+            System.setProperty("jexer.TWindow.opacity", "80");
+            System.setProperty("jexer.TImage.opacity", "80");
+            System.setProperty("jexer.TTerminal.opacity", "80");
+            System.setProperty("jexer.TButton.style", "round");
+
+            getTheme().setFemme();
+            for (TWindow window: getAllWindows()) {
+                window.setBorderStyleForeground("round");
+                window.setBorderStyleModal("round");
+                window.setBorderStyleMoving("round");
+                window.setBorderStyleInactive("round");
+                window.setAlpha(80 * 255 / 100);
+
+                for (TWidget widget: window.getChildren()) {
+                    if (widget instanceof TButton) {
+                        ((TButton) widget).setStyle(TButton.Style.ROUND);
+                    }
+                }
+            }
+            for (TMenu m: getAllMenus()) {
+                m.setBorderStyleForeground("round");
+                m.setBorderStyleModal("round");
+                m.setBorderStyleMoving("round");
+                m.setBorderStyleInactive("round");
+                m.setAlpha(90 * 255 / 100);
+            }
+            setDesktop(null);
+            setHideStatusBar(true);
+            return true;
+        }
+
+        if (menu.getId() == 10002) {
+            // Look bland: switch the color theme, window borders, and button
+            // styles.
+            System.clearProperty("jexer.TWindow.borderStyleForeground");
+            System.clearProperty("jexer.TWindow.borderStyleModal");
+            System.clearProperty("jexer.TWindow.borderStyleMoving");
+            System.clearProperty("jexer.TWindow.borderStyleInactive");
+            System.clearProperty("jexer.TEditColorTheme.borderStyle");
+            System.clearProperty("jexer.TEditColorTheme.options.borderStyle");
+            System.clearProperty("jexer.TRadioGroup.borderStyle");
+            System.clearProperty("jexer.TScreenOptions.borderStyle");
+            System.clearProperty("jexer.TScreenOptions.grid.borderStyle");
+            System.clearProperty("jexer.TScreenOptions.options.borderStyle");
+            System.clearProperty("jexer.TWindow.opacity");
+            System.clearProperty("jexer.TImage.opacity");
+            System.clearProperty("jexer.TTerminal.opacity");
+            System.clearProperty("jexer.TButton.style");
+
+            getTheme().setDefaultTheme();
+            for (TWindow window: getAllWindows()) {
+                window.setBorderStyleForeground(null);
+                window.setBorderStyleModal(null);
+                window.setBorderStyleMoving(null);
+                window.setBorderStyleInactive(null);
+                window.setAlpha(90 * 255 / 100);
+
+                for (TWidget widget: window.getChildren()) {
+                    if (widget instanceof TButton) {
+                        ((TButton) widget).setStyle(TButton.Style.SQUARE);
+                    }
+                }
+            }
+            for (TMenu m: getAllMenus()) {
+                m.setBorderStyleForeground(null);
+                m.setBorderStyleModal(null);
+                m.setBorderStyleMoving(null);
+                m.setBorderStyleInactive(null);
+                m.setAlpha(90 * 255 / 100);
+            }
+            setDesktop(new TDesktop(this));
+            setHideStatusBar(false);
+            return true;
+        }
+
+        if (menu.getId() == 10003) {
+            // Look "custom", sorta vaguely like Qmodem 5.
+            System.setProperty("jexer.TWindow.borderStyleForeground", "round");
+            System.setProperty("jexer.TWindow.borderStyleModal", "round");
+            System.setProperty("jexer.TWindow.borderStyleMoving", "round");
+            System.setProperty("jexer.TWindow.borderStyleInactive", "round");
+            System.setProperty("jexer.TEditColorTheme.borderStyle", "round");
+            System.setProperty("jexer.TEditColorTheme.options.borderStyle", "round");
+            System.setProperty("jexer.TRadioGroup.borderStyle", "round");
+            System.setProperty("jexer.TScreenOptions.borderStyle", "round");
+            System.setProperty("jexer.TScreenOptions.grid.borderStyle", "round");
+            System.setProperty("jexer.TScreenOptions.options.borderStyle", "round");
+            System.setProperty("jexer.TWindow.opacity", "90");
+            System.setProperty("jexer.TImage.opacity", "90");
+            System.setProperty("jexer.TTerminal.opacity", "90");
+            System.setProperty("jexer.TButton.style", "diamond");
+
+            getTheme().setQmodem5();
+            for (TWindow window: getAllWindows()) {
+                window.setBorderStyleForeground("round");
+                window.setBorderStyleModal("round");
+                window.setBorderStyleMoving("round");
+                window.setBorderStyleInactive("round");
+                window.setAlpha(90 * 255 / 100);
+
+                for (TWidget widget: window.getChildren()) {
+                    if (widget instanceof TButton) {
+                        ((TButton) widget).setStyle(TButton.Style.DIAMOND);
+                    }
+                }
+            }
+            for (TMenu m: getAllMenus()) {
+                m.setBorderStyleForeground("single");
+                m.setBorderStyleModal("single");
+                m.setBorderStyleMoving("single");
+                m.setBorderStyleInactive("single");
+                m.setAlpha(95 * 255 / 100);
+            }
+            setDesktop(new TDesktop(this));
+            setHideStatusBar(false);
+            return true;
+        }
+
+        if (menu.getId() == 10005) {
+            setLocale(Locale.forLanguageTag(""));
+            i18n = ResourceBundle.getBundle(DemoApplicationCore.class.getName(),
+                getLocale());
+            getBackend().setTitle(i18n.getString("applicationTitle"));
+            clearAllWidgets();
+            addAllWidgets();
+            return true;
+        }
+
+        if (menu.getId() == 10006) {
+            setLocale(Locale.forLanguageTag("es"));
+            i18n = ResourceBundle.getBundle(DemoApplicationCore.class.getName(),
+                getLocale());
+            getBackend().setTitle(i18n.getString("applicationTitle"));
+            clearAllWidgets();
+            addAllWidgets();
+            return true;
+        }
+
+        return super.onMenu(menu);
+    }
+
+    /**
+     * Show FPS.
+     */
+    @Override
+    protected void onPreDraw() {
+        if (getScreen() instanceof ECMA48Terminal) {
+            ECMA48Terminal terminal = (ECMA48Terminal) getScreen();
+            int bytes = terminal.getBytesPerSecond();
+            String bps = "";
+            if (bytes > 1024 * 1024 * 1024) {
+                bps = String.format("%4.2f GB/s",
+                    ((double) bytes / (1024 * 1024 * 1024)));
+            } else if (bytes > 1024 * 1024) {
+                bps = String.format("%4.2f MB/s",
+                    ((double) bytes / (1024 * 1024)));
+            } else if (bytes > 1024) {
+                bps = String.format("%4.2f KB/s",
+                    ((double) bytes / 1024));
+            } else {
+                bps = String.format("%d bytes/s", bytes);
+            }
+            menuTrayText = String.format("%s FPS %d", bps,
+                getFramesPerSecond());
+        } else {
+            menuTrayText = String.format("FPS %d", getFramesPerSecond());
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // DemoApplication --------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Add all the widgets of the demo.
+     */
+    private void addAllWidgets() {
+        new DemoCoreMainWindow(this);
+
+        // Add the menus
+        addToolMenu();
+        addFileMenu();
+        addEditMenu();
+
+        TMenu demoMenu = addMenu(i18n.getString("demo"));
+        demoMenu.addItem(10000, i18n.getString("mainWindow"));
+        demoMenu.addSeparator();
+        demoMenu.addItem(10001, i18n.getString("lookCute"));
+        demoMenu.addItem(10002, i18n.getString("lookBland"));
+        demoMenu.addItem(10003, i18n.getString("lookCustom"));
+        TSubMenu languageMenu = demoMenu.addSubMenu(i18n.getString("selectLanguage"));
+        languageMenu.addItem(10005, i18n.getString("english"));
+        languageMenu.addItem(10006, i18n.getString("espanol"));
+        demoMenu.addSeparator();
+        TMenuItem item = demoMenu.addItem(2000, i18n.getString("checkable"));
+        item.setCheckable(true);
+        item = demoMenu.addItem(2001, i18n.getString("disabled"));
+        item.setEnabled(false);
+        item = demoMenu.addItem(2002, i18n.getString("normal"));
+        TSubMenu subMenu = demoMenu.addSubMenu(i18n.getString("subMenu"));
+        item = demoMenu.addItem(2010, i18n.getString("normal"));
+        item = demoMenu.addItem(2050, i18n.getString("colors"));
+
+        item = subMenu.addItem(2000, i18n.getString("checkableSub"));
+        item.setCheckable(true);
+        item = subMenu.addItem(2001, i18n.getString("disabledSub"));
+        item.setEnabled(false);
+        item = subMenu.addItem(2002, i18n.getString("normalSub"));
+
+        subMenu = subMenu.addSubMenu(i18n.getString("subMenu"));
+        item = subMenu.addItem(2000, i18n.getString("checkableSub"));
+        item.setCheckable(true);
+        item = subMenu.addItem(2001, i18n.getString("disabledSub"));
+        item.setEnabled(false);
+        item = subMenu.addItem(2002, i18n.getString("normalSub"));
+
+        addTableMenu();
+        addWindowMenu();
+        addHelpMenu();
+    }
+
+    /**
+     * Clear all the widgets of the demo.
+     */
+    private void clearAllWidgets() {
+        closeMenu();
+        for (TMenu menu: getAllMenus()) {
+            removeMenu(menu);
+        }
+        for (TWindow window: getAllWindows()) {
+            closeWindow(window);
+        }
+    }
+
+}
